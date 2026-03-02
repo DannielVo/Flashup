ba73142844b2d48a951a12fb73ca1b37ce5e4c04
DannielVo <phattinhoc2017@gmail.com>
2026-03-02 16:51:25 +0700

    Feat: Finish UI and navigate route for Sidebar

---
+++ src/assets/assets.js
@@ -0,0 +1,36 @@
+import mainLogo from "./20-transparent.png";
+
+export const assets = { mainLogo };
+
+export const sidebarItems = [
+  {
+    id: 1,
+    icon: "chat_bubble",
+    path: "chats",
+    title: "Chats",
+  },
+  {
+    id: 2,
+    icon: "group",
+    path: "groups",
+    title: "Groups",
+  },
+  {
+    id: 3,
+    icon: "contact_page",
+    path: "contacts",
+    title: "Contacts",
+  },
+  {
+    id: 4,
+    icon: "notifications",
+    path: "notifications",
+    title: "Notifications",
+  },
+  {
+    id: 5,
+    icon: "settings",
+    path: "settings",
+    title: "Settings",
+  },
+];
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { assets, sidebarItems } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-20 flex flex-col py-8 items-center border-r border-slate-200 z-20">
+      {/* Logo */}
+      <div className="mb-10">
+        <img
+          src={assets.mainLogo}
+          alt="Flashup's Logo"
+          className="w-12 h-12 object-cover"
+        />
+      </div>
+
+      {/* Sidebar tabs */}
+      <div className="flex flex-col gap-6 flex-1">
+        {sidebarItems.map((item) => (
+          <NavLink
+            key={`sidebar-item-${item.id}`}
+            to={item.path}
+            className={({ isActive }) =>
+              `w-12 h-12 flex items-center justify-center rounded-xl ${isActive ? "bg-primary/10 text-primary" : "text-slate-400 hover:bg-primary/10 hover:text-primary"} transition-all duration-300 cursor-pointer`
+            }
+            title={item.title}
+          >
+            <span className="material-symbols-outlined active-icon">
+              {item.icon}
+            </span>
+          </NavLink>
+        ))}
+      </div>
+
+      {/* Logout */}
+      <button
+        title="Logout"
+        className="w-12 h-12 flex justify-center items-center rounded-xl text-slate-400 hover:text-red-500 hover:bg-red-100 transition-all duration-300 cursor-pointer"
+      >
+        <span className="material-symbols-outlined">logout</span>
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
