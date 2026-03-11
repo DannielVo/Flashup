4f460982e01594da2af10ac035db66bfc79b27de
DannielVo <phattinhoc2017@gmail.com>
2026-03-11 09:52:31 +0700

    Fix: Update text color of Not found page

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -7,13 +7,13 @@ const NotFound = () => {
   return (
     <div className="h-screen flex items-center justify-center mx-auto max-w-7xl lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-sm text-center">
-        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">
           404
         </h1>
-        <p className="mb-5 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+        <p className="mb-5 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">
           Something's missing.
         </p>
-        <p className="mb-5 text-lg font-light text-gray-500 dark:text-gray-400">
+        <p className="mb-5 text-lg font-light text-gray-500">
           Sorry, we can't find that page. You'll find lots to explore on the
           home page.{" "}
         </p>
