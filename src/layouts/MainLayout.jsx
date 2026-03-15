d1e82e47af6f8d5bd65f47ecffe51714e34e66a9
DannielVo <phattinhoc2017@gmail.com>
2026-03-15 10:25:54 +0700

    Fix: Update sub-sidebar layout, chat window layout and modal in MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
-import ChatWindow from "../components/chat/ChatWindow";
+import useMobileNav from "../hooks/useMobileNav";
+import MobileSidebarOverlay from "../components/sidebar/MobileSidebarOverlay";
+import ChatWindowLayout from "../components/chat/ChatWindowLayout";
+import AddGroupModal from "../components/group/AddGroupModal";
 
 const MainLayout = () => {
+  const { isMobileNavOpen, toggleMobileNav, closeMobileNav } = useMobileNav();
+  const [selectedChatId, setSelectedChatId] = useState(1);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [activeModal, setActiveModal] = useState(null);
+
   return (
-    <div className="flex bg-background-light h-screen overflow-hidden text-slate-800">
+    <div className="flex h-screen overflow-hidden text-slate-800">
+      {/* SIDEBAR */}
       <Sidebar />
-      <Outlet />
-      <ChatWindow />
+
+      {/* SUB-SIDEBAR */}
+      <div
+        className={`${isChatOpen ? "hidden" : "flex"} lg:flex flex-1 flex-col lg:w-80 lg:flex-0 bg-white`}
+      >
+        <Outlet
+          context={{
+            isMobileNavOpen,
+            toggleMobileNav,
+            closeMobileNav,
+            selectedChatId,
+            setSelectedChatId,
+            setIsChatOpen,
+            setActiveModal,
+          }}
+        />
+      </div>
+
+      {/* CHATWINDOW */}
+      <div
+        className={`${isChatOpen ? "flex" : "hidden"} lg:flex flex-1 flex-col bg-background-light`}
+      >
+        <ChatWindowLayout
+          selectedChatId={selectedChatId}
+          setIsChatOpen={setIsChatOpen}
+        />
+      </div>
+
+      {/* SIDEBAR FOR TABLET + MOBILE */}
+      <MobileSidebarOverlay isOpen={isMobileNavOpen} onClose={closeMobileNav} />
+
+      {/* ============= MODAL ============= */}
+
+      {/* ADD GROUP MODAL */}
+      <AddGroupModal
+        isOpen={activeModal === "add-group"}
+        onClose={() => setActiveModal(null)}
+      />
+
+      {/* *CÁC MODAL DƯỚI ĐÂY LÀM TƯƠNG TỰ ADD GROUP MODAL* */}
+
+      {/* ADD CONTACT MODAL */}
+
+      {/* PROFILE MODAL */}
+
+      {/* CHANGE PASSWORD MODAL */}
+
+      {/* {activeModal === "add-group" && (
+        <AddGroupModal onClose={() => setActiveModal(null)} />
+      )} */}
     </div>
   );
 };
