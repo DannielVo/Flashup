6bc3e05a601d23799c2986a5f9b7e70c0ab325a4
DannielVo <phattinhoc2017@gmail.com>
2026-03-19 13:06:53 +0700

    Feat: Add Chat info panel in ChatWindowLayout

--- src/components/chat/ChatWindowLayout.jsx
+++ src/components/chat/ChatWindowLayout.jsx
@@ -1,15 +1,29 @@
 import ChatWindowHeader from "./ChatWindowHeader";
 import ChatWindowBody from "./ChatWindowBody";
 import ChatWindowFooter from "./ChatWindowFooter";
+import { useState } from "react";
+import { LIST_CHATS } from "../../assets/dummyDB";
+import ChatInfoPanel from "./ChatInfoPanel";
 
 const ChatWindowLayout = ({ selectedChatId, setIsChatOpen }) => {
+  const [isInfoPanelOpen, setIsInfoPanelOpen] = useState(false);
+
+  const chat = LIST_CHATS.find((c) => c.id === selectedChatId);
+
   return (
     <>
       <ChatWindowHeader
         selectedChatId={selectedChatId}
         setIsChatOpen={setIsChatOpen}
+        onOpenInfoPanel={() => setIsInfoPanelOpen(true)}
       />
       <ChatWindowBody selectedChatId={selectedChatId} />
+
+      {/* CHAT INFO PANEL */}
+      {isInfoPanelOpen && (
+        <ChatInfoPanel chat={chat} onClose={() => setIsInfoPanelOpen(false)} />
+      )}
+
       <ChatWindowFooter />
     </>
   );
